484f6aa6f912ec4464f5b319710f476322b6141d
aizaz kotwal <aizazkotwal02@gmail.com>
2021-05-18 15:44:52 +0530

    added the collapsible component and added the js code for the collapsible

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+let collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("collapsible--expanded");
+  });
+});
